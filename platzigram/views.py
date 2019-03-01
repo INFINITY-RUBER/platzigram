2d077f38cdae2fc52a1ad1234e5ea7d9bfbc4545
RUBER <ruberhenandez@gmail.com>
2019-02-28 20:50:04 -0500

    creamos app para los usuarios Se modifica y model.py y user.py luego se hace migracion

--- platzigram/views.py
+++ platzigram/views.py
@@ -1,33 +1,39 @@
-""" platzigram views."""
-#Django
+"""Platzigram views."""
+
+# Django
 from django.http import HttpResponse
-# utilidades
+
+# Utilities
 from datetime import datetime
 import json
 
+
 def hello_world(request):
-    #now = datetime.now().strftime('%b %dht, %Y - %H:%M hrs')
-    return HttpResponse('hola ruber! esta es la hora {now}'.format(
-		now=datetime.now().strftime('%b - %d - %Y : %H:%M hrs')
-	))
+    """Return a greeting."""
+    return HttpResponse('Oh, hi! Current server time is {now}'.format(
+        now=datetime.now().strftime('%b %dth, %Y - %H:%M hrs')
+    ))
 
 
-def sort_intergers(request):
-    """ Resturn  a JSON response with """
-    numbers = [ int(i) for i in request.GET["numbers"].split(",") ]
+def sort_integers(request):
+    """Return a JSON response with sorted integers."""
+    numbers = [int(i) for i in request.GET['numbers'].split(',')]
     sorted_ints = sorted(numbers)
     data = {
         'status': 'ok',
-        'number': sorted_ints,
-        'message': 'integers sorted successfully'
+        'numbers': sorted_ints,
+        'message': 'Integers sorted successfully.'
     }
-    return HttpResponse(json.dumps(data, indent=4), content_type='application/json')
+    return HttpResponse(
+        json.dumps(data, indent=4),
+        content_type='application/json'
+    )
 
 
 def say_hi(request, name, age):
-    """Returana a greeting. """
+    """Return a greeting."""
     if age < 12:
-        messege = 'Sorry  {},  you are not allowed hare'.format(name)
+        message = 'Sorry {}, you are not allowed here'.format(name)
     else:
-        messege = 'Hello Ruber  {}, Bienvenido '.format(name)
-    return HttpResponse(messege)
+        message = 'Hello, {}! Welcome to Platzigram'.format(name)
+    return HttpResponse(message)
