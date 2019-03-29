e55c7af3e648efb29410adbe3abba6bb95d22fbf
RUBER <ruberhenandez@gmail.com>
2019-03-28 22:09:24 -0500

    Clase 31 se utiliza vistas genéricas basadas en clases y usa las de la documentacion

--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import views as auth_views
 from django.shortcuts import render, redirect
 from django.urls import reverse, reverse_lazy
 from django.views.generic import DetailView, FormView, UpdateView
@@ -64,21 +65,14 @@ class UpdateProfileView(LoginRequiredMixin, UpdateView):
         return reverse('users:detail', kwargs={'username': username})
 
 
-def login_view(request):
+class LoginView(auth_views.LoginView):
     """Login view."""
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user:
-            login(request, user)
-            return redirect('posts:feed')
-        else:
-            return render(request, 'users/login.html', {'error': 'Invalid username and password'})
-
-    return render(request, 'users/login.html')
+    template_name = 'users/login.html'
 
 
+class LogoutView(LoginRequiredMixin, auth_views.LogoutView):
+    """Logout view."""
+    template_name = 'users/login.html'
 
 
 @login_required
